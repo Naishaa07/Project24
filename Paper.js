da2210dabd8565d9f42bb1ca3aaacb3b94209352
Naishaa07 <72614914+Naishaa07@users.noreply.github.com>
2020-11-11 11:36:15 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+    constructor(x,y,r){
+        var options={
+            restitution:0.3,
+            friction:0.5,
+            density:1.2,
+            
+
+
+        }
+        this.y=y
+        this.x=x
+        this.r=r
+        this.body=Bodies.circle(x,y,r/2,options);
+        World.add(world,this.body)
+    }
+    display(){
+        var pos=this.body.position
+        var angle=this.body.angle
+        push ();
+        translate(pos.x,pos.y)
+        rotate (angle)
+        rectMode(CENTER)
+        
+        fill (255,0,255)
+        ellipse(0,0,this.r,this.r)
+        pop ();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground, paper, db1, db2, db3;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground=new Ground(400,690,800,20);
+	paper = new Paper(50,675,30);
+	db1 = new dustbin(675,670,200,20);
+	db2 = new dustbin(565,630,20,100);
+	db3 = new dustbin(785,630,20, 100)
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  paper.display();
+  db1.display();
+  db3.display();
+  db2.display();
+  //drawSprites();
+ keypressed();
+}
+function keypressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:85,y:-85});
+		
+	}
+
+
+}
+
+
+
